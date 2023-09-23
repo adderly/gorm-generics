6730c19cbaeb4299496b0978d42b9ee783a65931
adderly <adderlygonzalez@gmail.com>
2023-09-23 10:14:20 -0400

    * Added method for mapping model to dto

--- utils.go
+++ utils.go
@@ -16,3 +16,22 @@ func ChunkSlice[T any](slice []T, chunkSize int) [][]T {
 
 	return chunks
 }
+
+func MapDto[M GormModel[E], E any, T any](modelArray []M, dtoType T) []T {
+	return Map(modelArray, func(ce M) T {
+		et := ce.ToEntity()
+		etCasted, ok := any(et).(T)
+		if !ok {
+			return *new(T)
+		}
+		return etCasted
+	})
+}
+
+func Map[T, U any](ts []T, f func(T) U) []U {
+	us := make([]U, len(ts))
+	for i := range ts {
+		us[i] = f(ts[i])
+	}
+	return us
+}
