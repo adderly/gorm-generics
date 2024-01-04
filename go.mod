f4eee5bbaff49b6d00bf3636612f7cfddda4bfd1
adderly <adderlygonzalez@gmail.com>
2024-01-04 07:52:40 -0400

    * Added paging options for flexibility (WIP: testing needed)

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.21
 
 require (
 	gorm.io/driver/sqlite v1.4.3
-	gorm.io/gorm v1.24.0
+	gorm.io/gorm v1.25.5
 )
 
 require (
--- repository.go
+++ repository.go
@@ -2,6 +2,7 @@ package gorm_generics
 
 import (
 	"context"
+	"math"
 
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
@@ -10,6 +11,22 @@ import (
 type GormModel[E any] interface {
 	ToEntity() E
 	FromEntity(entity E) interface{}
+}
+
+type PageResult[M GormModel[E], E any] struct {
+	Data  []M   `json:"data"`
+	Count int64 `json:"count"`
+	Page  int   `json:"page"`
+}
+
+type PageConfig struct {
+	Page int   `json:"page"`
+	Size int64 `json:"size"`
+	// By default the count is generated when asking for the first page so that the user
+	// receives the total amount, but with this we can optimize out this count
+	IgnoreCount bool `json:"IngoreCount"`
+	// if you want the count to always be returned.
+	ForceCount bool `json:"ForceCount"`
 }
 
 func NewRepository[M GormModel[E], E any](db *gorm.DB) *GormRepository[M, E] {
@@ -136,6 +153,10 @@ func (r *GormRepository[M, E]) Find(ctx context.Context, specifications ...Speci
 	return r.FindWithLimit(ctx, -1, -1, specifications...)
 }
 
+func (r *GormRepository[M, E]) FindPaged(ctx context.Context, specifications ...Specification) ([]E, error) {
+	return r.FindWithLimit(ctx, -1, -1, specifications...)
+}
+
 func (r *GormRepository[M, E]) Count(ctx context.Context, specifications ...Specification) (i int64, err error) {
 	model := new(M)
 	err = r.getPreWarmDbForSelect(ctx, specifications...).Model(model).Count(&i).Error
@@ -143,7 +164,7 @@ func (r *GormRepository[M, E]) Count(ctx context.Context, specifications ...Spec
 }
 
 func (r *GormRepository[M, E]) getPreWarmDbForSelect(ctx context.Context, specification ...Specification) *gorm.DB {
-	dbPrewarm := r.db.WithContext(ctx)
+	var dbPrewarm *gorm.DB = r.db.WithContext(ctx)
 	for _, s := range specification {
 		dbPrewarm = dbPrewarm.Where(s.GetQuery(), s.GetValues()...)
 	}
@@ -166,6 +187,46 @@ func (r *GormRepository[M, E]) FindWithLimit(ctx context.Context, limit int, off
 	}
 
 	return result, nil
+}
+
+func (r *GormRepository[M, E]) FindPagedWithLimit(ctx context.Context, pageCfg PageConfig, specifications ...Specification) (PageResult[M, E], error) {
+	var models []M
+	dbPrewarm := r.getPreWarmDbForSelect(ctx, specifications...)
+
+	//If page is 0 do the count
+	rs := PageResult[M, E]{
+		Count: 0,
+		Page:  pageCfg.Page,
+	}
+
+	minLimit := math.Max(1, float64(pageCfg.Size))
+	shouldCount := pageCfg.ForceCount || (pageCfg.Page == 0 && !pageCfg.IgnoreCount)
+
+	if shouldCount {
+		model := new(M)
+
+		var elementCount int64 = 0
+		er := dbPrewarm.Model(model).Count(&elementCount)
+
+		if er.Error != nil {
+			return rs, er.Error
+		}
+		rs.Count = int64(math.Ceil(float64(elementCount) / float64(minLimit)))
+	}
+
+	err := dbPrewarm.Limit(int(minLimit)).Offset(pageCfg.Page).Find(&models).Error
+
+	if err != nil {
+		return rs, err
+	}
+
+	// result := make([]E, 0, len(models))
+	// for _, row := range models {
+	// 	result = append(result, row.ToEntity())
+	// }
+
+	rs.Data = models
+	return rs, nil
 }
 
 func (r *GormRepository[M, E]) FindAll(ctx context.Context) ([]E, error) {
